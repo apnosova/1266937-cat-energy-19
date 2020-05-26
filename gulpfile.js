0060b35479ed87925ba402653c994bac32ad23ff
apnosova <angelinanosova@yandex.ru>
2020-05-26 08:47:38 +0300

    Защита проекта

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,17 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var server = require("browser-sync").create();
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+
+var posthtml = require("gulp-posthtml");
+var beautify = require("posthtml-beautify");
+var del = require ("del");
+
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -17,13 +24,17 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe (rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -32,7 +43,40 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+  beautify()
+  ]))
+  .pipe(gulp.dest("build"));
+});
+
+
+gulp.task("copy", function() {
+  return gulp.src ([
+    "source/fonts/**/*.{woff, woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ],{
+  base: "source"
+})
+.pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("refresh", function(done) {
+  server.reload();
+  done();
+})
+
+gulp.task("build", gulp.series("clean","copy","css", "html"));
+gulp.task("start", gulp.series("build", "server"));
